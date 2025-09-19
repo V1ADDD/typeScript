85677c68543bf18fefc9c92f86b507724078032a
Vlad <rusakvlad2@gmail.com>
2025-09-19 16:45:47 +0300

    feat: any, unknown, never

--- typescript/src/app/main/main.ts
+++ typescript/src/app/main/main.ts
@@ -7,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './main.scss'
 })
 export class Main {
+  first() {
+    const parentElement = document.getElementById("main");
 
+    // Any
+    let first: any = 123;
+    first = "First";
+
+    //Unknown
+    const parseData = (data: string): unknown => JSON.parse(data);
+    const jsonString = '{"name": "Vlad"}';
+    const parsed = parseData(jsonString);
+
+    //Never
+    const thrErr = (message: string): never => { throw new Error(message) };
+  }
 }
