0132dcba32bedb43d2a962a0a7021d8ed72a8a16
Vlad <rusakvlad2@gmail.com>
2025-09-22 12:55:43 +0300

    feat: generics, narrowing, default values

--- typescript/src/app/main/main.ts
+++ typescript/src/app/main/main.ts
@@ -21,5 +21,34 @@ export class Main {
 
     //Never
     const thrErr = (message: string): never => { throw new Error(message) };
+
+
+    // Generics
+    function getFirst<T>(arr: T[]): T {
+      return arr[0];
+    }
+    const str = getFirst(["str1","str2"]); // string
+    const num = getFirst([1,2,3]) // number
+
+    // Type narrowing
+    function getValue(val: string | number): number{
+      if (typeof val === "string") {
+        return val.length;
+      }
+      else {
+        return val / 100; // narrowed to number
+      }
+    }
+    const len = getValue(str);
+    const perc = getValue(num);
+
+    // Default types
+    function processData<T = string>(data: T): T {
+      return data;
+    }
+    processData("Hello World"); //string
+
+    
+
   }
 }
