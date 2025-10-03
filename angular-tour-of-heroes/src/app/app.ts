9d0473b4d6a982f4cca11c160f57920f738fa881
Vlad <rusakvlad2@gmail.com>
2025-10-03 13:00:21 +0300

    feat: initializing app

--- angular-tour-of-heroes/src/app/app.ts
+++ angular-tour-of-heroes/src/app/app.ts
@@ -7,5 +7,5 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('angular-tour-of-heroes');
+  protected readonly title = signal('Tour of Heroes');
 }
